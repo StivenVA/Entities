c8c053c80e2c48e78ba7168db35a3cac513560bf
stivenVA <stivenv.ariza@gmail.com>
2024-10-09 16:00:27 -0500

    refactor: DetailRepository method save

--- src/main/java/org/clubhive/repositories/implement/DetailRepository.java
+++ src/main/java/org/clubhive/repositories/implement/DetailRepository.java
@@ -1,11 +1,15 @@
 package org.clubhive.repositories.implement;
 
 import FindUser.FindUser;
+import exceptions.NoBugsException;
 import lombok.RequiredArgsConstructor;
 import org.clubhive.entities.DetailEntity;
 import org.clubhive.model.Detail;
 import org.clubhive.repositories.jpa.DetailRepositoryJpa;
+import org.clubhive.utils.BuyMapper;
 import org.clubhive.utils.DetailMapper;
+import org.clubhive.utils.TicketMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,8 +23,18 @@ public class DetailRepository {
     private final DetailRepositoryJpa detailRepository;
 
     public Detail save(Detail detail) {
+
+        if (detail.getIdBuy() == null)
+            throw new NoBugsException("Buy must not be null", HttpStatus.BAD_REQUEST);
+
+        DetailEntity detailEntity = new DetailEntity();
+
+        detailEntity.setIdBuyEntity(BuyMapper.mapToBuyEntity(detail.getIdBuy()));
+        detailEntity.setQuantity(detail.getQuantity());
+        detailEntity.setIdTicket(TicketMapper.modelToEntity(detail.getIdTicket()));
+        
         return DetailMapper
-                .mapToDetail(detailRepository.save(DetailMapper.mapToDetailEntity(detail)));
+                .mapToDetail(detailRepository.save(detailEntity));
     }
 
     public Detail findById(Long id) {
